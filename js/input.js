584ad40993f02d3950dfa3afe89e1607c8dc5475
jesse-y <jesse.yue@gmail.com>
2017-07-09 14:18:46 +1000

    camera panning now available. input.js modified to remove anonymous function initialisaton. added new SceneCamera controller instead

--- js/input.js
+++ js/input.js
@@ -1,4 +1,117 @@
-(function () {
+function SceneCamera(camera, render_func) {
+	//button code for mouse clicks. 
+	//IE8 returns 1, 4, 2 for left, middle and right buttons respectively.
+	var l_btn = 0;
+	var m_btn = 1;
+	var r_btn = 2;
+	const default_btn_state = [false, 0, 0, 0, 0];
+
+	var theta, phi, r;
+	var target;
+	var mouse = {};
+	var prev_camera;
+	var prev_target;
+
+	r = 150;
+	theta = 70;
+	phi = 70;
+
+	mouse[l_btn] = default_btn_state;
+	mouse[m_btn] = default_btn_state;
+	mouse[r_btn] = default_btn_state;
+
+	target = new THREE.Object3D();
+	prev_target = target.clone();
+
+	function mouse_down(event) {
+		event.preventDefault();
+		set_mouse(event, true);
+
+		prev_target = target.clone();
+	}
+
+	function mouse_up(event) {
+		event.preventDefault();
+		set_mouse(event, false);
+	}
+
+	function set_mouse(event, status) {
+		var btn = event.button;
+		mouse[btn] = [status, event.clientX, event.clientY, theta, phi];
+	}
+
+	function mouse_move(event) {
+		event.preventDefault();
+		var btn = event.button;
+
+		if (mouse[btn][0]) {
+			if (btn == l_btn) {
+				orbit_camera(mouse[btn], event);
+			} else if (btn == m_btn) {
+				pan_camera(mouse[btn], event);
+			}
+		}
+	}
+
+	function pan_camera(btn_state, event) {
+		var click_x = btn_state[1];
+		var click_y = btn_state[2];
+
+		var offset_x = -((event.clientX - click_x) * 0.2);
+		var offset_y = ((event.clientY - click_y) * 0.2);
+
+		target.position.copy(prev_target.position);
+
+		target.translateX(offset_x);
+		target.translateY(offset_y);
+
+		set_camera_pos();
+		render_func();
+	}
+
+	function orbit_camera(btn_state, event) {
+		var click_x = btn_state[1];
+		var click_y = btn_state[2];
+		var click_theta = btn_state[3];
+		var click_phi = btn_state[4];
+
+		theta = - ((event.clientX - click_x) * 0.5) + click_theta;
+		phi = ((event.clientY - click_y) * 0.5) + click_phi;
+
+		if (theta < -Math.PI * 360) theta += 2*Math.PI * 360;
+		if (theta > Math.PI * 360) theta -= 2*Math.PI * 360;
+		if (phi > 180) phi = 180;
+		if (phi < -180) phi = -180;
+
+		set_camera_pos();
+		render_func();
+	}
+
+	function set_camera_pos() {
+		camera.position.x = (r * Math.sin( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 )) + target.position.x;
+		camera.position.y = (r * Math.sin( phi * Math.PI / 360 )) + target.position.y;
+		camera.position.z = (r * Math.cos( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 )) + target.position.z;
+		camera.updateMatrix();
+
+		console.log(theta, phi, camera.position);
+
+		camera.lookAt(target.position);
+		target.lookAt(camera.position);
+	}
+
+	this.init = function() {
+		document.addEventListener('mousemove', mouse_move, false );
+		document.addEventListener( 'mousedown', mouse_down, false );
+		document.addEventListener( 'mouseup', mouse_up, false );
+
+		set_camera_pos();
+		render_func();
+	}
+
+	this.init();
+}
+
+/*(function () {
 	//button code for mouse clicks. 
 	//IE8 returns 1, 4, 2 for left, middle and right buttons respectively.
 	var l_btn = 0;
@@ -118,3 +231,4 @@
 		}
 	};
 })();
+*/
--- js/viewport.js
+++ js/viewport.js
@@ -12,10 +12,6 @@ function init() {
 
 	//setup camera
 	camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 1000);
-	camera.position.x = 150 * Math.sin( 60 * Math.PI / 360 ) * Math.cos( 45 * Math.PI / 360 );
-	camera.position.y = 150 * Math.sin( 45 * Math.PI / 360 );
-	camera.position.z = 150 * Math.cos( 60 * Math.PI / 360 ) * Math.cos( 45 * Math.PI / 360 );
-	camera.lookAt(new THREE.Vector3());
 
 	//setup scene
 	scene = new THREE.Scene();
@@ -40,91 +36,17 @@ function init() {
 	vport_div.appendChild( renderer.domElement );
 
 	//bind input controller
-	controls = new Controller(camera);
-	document.addEventListener('mousemove', controls.update);
+	controls = new SceneCamera(camera, render);
 
 	//handle resizing
 	window.addEventListener( 'resize', onWindowResize, false );
 
 	var loc = new THREE.Object3D();
-	loc.position.set(0,10,0);
+	loc.position.set(0,0,0);
 	loc.add(new THREE.AxisHelper(10));
 	scene.add(loc);
 }
 
-function Controller(camera) {
-	var x, y, z;
-	var theta, phi, r;
-
-	r = 150;
-	theta = 60;
-	phi = 45;
-
-	var isMouseDown, onMouseDownTheta, onMouseDownPhi, onMouseDownPosition;
-	onMouseDownPosition = { x:0, y: 0 };
-	onMouseDownTheta = theta;
-	onMouseDownPhi = phi;
-
-	function onDocumentMouseDown( event ) {
-
-		event.preventDefault();
-
-		isMouseDown = true;
-
-		onMouseDownTheta = theta;
-		onMouseDownPhi = phi;
-		onMouseDownPosition.x = event.clientX;
-		onMouseDownPosition.y = event.clientY;
-
-	}
-
-	function onDocumentMouseMove( event ) {
-
-		event.preventDefault();
-
-		if ( isMouseDown ) {
-
-			theta = - ( ( event.clientX - onMouseDownPosition.x ) * 0.5 ) + onMouseDownTheta;
-			phi = ( ( event.clientY - onMouseDownPosition.y ) * 0.5 ) + onMouseDownPhi;
-
-			if (theta < -Math.PI * 360) theta += 2*Math.PI * 360;
-			if (theta > Math.PI * 360) theta -= 2*Math.PI * 360;
-			if (phi > 180) phi = 180;
-			if (phi < -180) phi = -180;
-
-			//phi = Math.min( 180, Math.max( 0, phi ) );
-
-			camera.position.x = r * Math.sin( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
-			camera.position.y = r * Math.sin( phi * Math.PI / 360 );
-			camera.position.z = r * Math.cos( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
-			camera.updateMatrix();
-
-			console.log(theta, phi, camera.position);
-
-			camera.lookAt(new THREE.Vector3());
-		}
-
-		render();
-	}
-
-	function onDocumentMouseUp( event ) {
-
-		event.preventDefault();
-
-		isMouseDown = false;
-
-		onMouseDownPosition.x = event.clientX - onMouseDownPosition.x;
-		onMouseDownPosition.y = event.clientY - onMouseDownPosition.y;
-	}
-
-	this.init = function() {
-		document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-		document.addEventListener( 'mousedown', onDocumentMouseDown, false );
-		document.addEventListener( 'mouseup', onDocumentMouseUp, false );
-	}
-
-	this.init();
-}
 
 function render() {
 	renderer.render(scene, camera);
