daaecd4f0008584a4f8e9cd4e30dd416edc546e7
jesse-y <jesse.yue@gmail.com>
2017-08-03 19:51:16 +1000

    comments added. base configuration of the robot updated.

--- js/workspace.js
+++ js/workspace.js
@@ -41,22 +41,25 @@ function init() {
 	//create end effector target
 	ee_target = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, 2),
 							   new THREE.MeshBasicMaterial({color:0xff0000, transparent:true,  opacity:0.5}));
-	ee_target.position.set(15, 10, 0);
+	ee_target.position.set(8, 17, 0);
 	manipulator.attach(ee_target);
 
 	scene.add(manipulator);
 	scene.add(ee_target);
 
-	//create kinematic chain
+	//create kinematic chain. the chain is an N * 4 array where N
+	//is the number of joints, and each entry refers to the 4 DH
+	//parameters in this order: d, a, alpha, theta.
 	chain = new KinematicChain([
 		[5, 0, -90, 0],
-		[0, 5, 90, 125],
+		[0, 2, 90, 125],
 		[0, 15, 0, -35],
-		[15, 5, 90, 0],
+		[15, 2, 90, 0],
 		[0, 0, -90, 90],
-		[5, 0, -90, 0]
+		[2, 0, -90, 0]
 	]);
 
+	//add the chain's objects to the scene for visualisation.
 	chain.get_all_meshes().forEach(function(m) {
 		scene.add(m);
 	});
@@ -72,22 +75,24 @@ function animate() {
 		dist_to_target = Math.abs(ee_pos.clone().sub(target).length());
 
 	if (dist_to_target > 0.5) {
+		//determine the gradient for each joint
 		angles = chain.iterateIK(target);
 
-		var max_speed = 10,
-			rotation_speed = Math.log(dist_to_target);
-
+		//determine scaling factor for the speed at which the
+		//joint should move.
+		var rotation_speed = Math.log(dist_to_target);
 		rotation_speed = Math.max(rotation_speed, 1);
 
-		console.log(`max speed = ${max_speed}, rotation_speed = ${rotation_speed}`);
+		console.log(`rotation_speed = ${rotation_speed}, dist_to_target=${dist_to_target}`);
 
+		//modify the chain to move each joint down its gradient, and
+		//then recalculate the position of the chain's end effector.
 		for (var i = 0; i < chain.theta.length; i++) {
 			chain.theta[i] += (angles[i]*rotation_speed) * -1;
 		}
 
 		chain.forward();
 	}
-
 
 	render();
 	window.setTimeout(animate, 50);
